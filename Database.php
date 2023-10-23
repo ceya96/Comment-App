693f62f357cb1cf655a06a6406715e152e62eafd
Yasin Aladi <yasin@oveleon.de>
2023-10-23 17:20:36 +0200

    Update database

--- Database.php
+++ Database.php
@@ -8,25 +8,12 @@ class Database
     {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $this->connection = new mysqli("localhost", "root", "", "kommentare");
-        if ($this->connection->connect_error)
+        if ($this->connection->error)
         {
-            throw new Exception('Es konnte keine Verbindung hergestellt werden');
-            exit();
+            exit('Es konnte keine Verbindung hergestellt werden');
         }
     }
-    public function insert($name, $email, $text, $parentId)
-    {
-        $sql = $this->connection->prepare("INSERT INTO kommentare (name, email, text, pid) VALUES (?,?,?,?)");
-        $sql->bind_param('sssi', $name, $email, $text, $parentId);
-        $sql->execute();
-
-        ($this->connection)->close();
-    }
-    public function select()
-    {
-        $sql = $this->connection->prepare("SELECT email, name, text, id, pid, tstamp FROM kommentare ORDER BY tstamp DESC ");
-        $sql->execute();
-        $result = $sql->get_result();
-        return $result;
-    }
 }
+
+// (!!) Datenbank kennt keine Komentare
+//Bei Login will ich die gleichen Methode nutzen aber User Daten 'inserten' ->ANPASSEN
