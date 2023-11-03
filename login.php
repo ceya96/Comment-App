ded9b92de708ebbfae0ff80c46d3883a108e3563
Yasin Aladi <yasin@oveleon.de>
2023-11-03 15:42:34 +0100

    fixed title

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $_SESSION['username'] = "";
     <link rel="stylesheet" href="registration.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Login</title>
 </head>
 <body>
 
--- regisSuccess.php
+++ regisSuccess.php
@@ -10,7 +10,7 @@ if(isset($_POST['toLogin']))
     <link rel="stylesheet" href="registration.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Registration erfolgreich</title>
 </head>
 <body>
 
